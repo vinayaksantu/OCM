b8c2d7d30012c4ece21107875f074bfd9e688a8a
admin <admin@DESKTOP-O6A5C2O>
2020-01-20 16:24:17 +0530

    commit Extent report

--- ocms/src/main/java/com/tetherfi/ExtentReportListener/ExtentReporterNG.java
+++ ocms/src/main/java/com/tetherfi/ExtentReportListener/ExtentReporterNG.java
@@ -25,7 +25,7 @@ public class ExtentReporterNG implements IReporter{
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
 			String outputDirectory) {
 		extent = new ExtentReports(outputDirectory + File.separator
-				+ "Extent.html", true);
+				+ "ExtentReportTestResults.html", false);
 
 		for (ISuite suite : suites) {
 			Map<String, ISuiteResult> result = suite.getResults();
